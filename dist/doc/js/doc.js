547b2ddae478947b9ba0dd22a85ace2b71d2da0d
g-milligan <gmilligan@human-element.com>
2015-04-30 19:57:38 -0400

    shader notes

--- dist/doc/js/doc.js
+++ dist/doc/js/doc.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function(){
+  //for each div.docWrap, not already initialized
   var wraps=jQuery('.docWrap').not('.init');
   wraps.each(function(){
     var wrap=jQuery(this); wrap.addClass('init');
